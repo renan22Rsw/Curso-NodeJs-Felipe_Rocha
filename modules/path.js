a8059fd8c1f2b7f07cfbfd82764681bb405eeec2
Renan Victor <renan22rsw@gmail.com>
2024-10-17 18:33:14 -0300

    Path Module

---
+++ modules/path.js
@@ -0,0 +1,21 @@
+import path from "path";
+
+//Mostra o nome do arquivo atual
+
+console.log(path.basename(__filename));
+
+//Mostra o nome do diretório atual
+
+console.log(path.dirname(__filename));
+
+//Mostra extenção do arquivo
+
+console.log(path.extname(__filename));
+
+// Cria objeto path
+
+console.log(path.parse(__filename));
+
+// Juntar caminhos de arquivos
+
+console.log(path.join(__dirname, "test"));
