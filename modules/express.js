e799985eed6cfd51d5f68a3af16c53085fc95890
Renan Victor <renan22rsw@gmail.com>
2024-10-24 20:53:53 -0300

    deleteUserById

--- modules/express.js
+++ modules/express.js
@@ -54,4 +54,15 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+//deleteUserById
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await UserModel.findByIdAndDelete(req.params.id);
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
 app.listen(PORT, () => console.log(`rodando com express na porta: ${PORT}`));
